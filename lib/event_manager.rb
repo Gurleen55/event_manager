233c3ca6dd2e7abec115e0af3adb827467484223
Gurleen <gurleensinghdalli@gmail.com>
2024-07-07 15:52:09 -0300

    time targeting and day targetting implemented both the features

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,6 +3,10 @@ require 'google/apis/civicinfo_v2'
 require 'erb'
 require 'time'
 
+# initializing variables outside of the blocks so they could be accessible everywhere
+hours = []
+j = 0
+days = []
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,'0')[0..4]
@@ -34,41 +38,6 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
-puts 'Event Manager Initialized!'
-
-# lines = File.readlines('event_attendees.csv')
-# row_index = 0
-# lines.each_with_index do |line, index|
-#   next if index == 0
-#   columns = line.split(",")
-#   name = columns[2]
-#   puts name
-# end
-
-contents = CSV.open(
-  'event_attendees.csv', 
-  headers: true,
-  header_converters: :symbol
-)
-
-# template_letter = File.read('form_letter.erb')
-# erb_template = ERB.new template_letter
-# contents.each do |row|
-
-#   id = row[0] 
-
-#   name = row[:first_name]
-
-#   zipcode = clean_zipcode(row[:zipcode])
-
-#   legislators = legislators_by_zipcode(zipcode)
-
-#   form_letter = erb_template.result(binding)
-
-#   save_thank_you_letter(id, form_letter)
-
-# end
-
 def clean_phone_number(phone_number)
   phone_number.each_char.with_index do |char, index|
     unless phone_number[index].to_i.to_s == phone_number[index]
@@ -97,16 +66,69 @@ def fix_time(time)
   parts.map {|part| part.rjust(2, "0")}.join(':')
 end
 
-hour = []
-j = 0
+puts 'Event Manager Initialized!'
+
+# lines = File.readlines('event_attendees.csv')
+# row_index = 0
+# lines.each_with_index do |line, index|
+#   next if index == 0
+#   columns = line.split(",")
+#   name = columns[2]
+#   puts name
+# end
+
+contents = CSV.open(
+  'event_attendees.csv', 
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
 contents.each do |row|
+
+  id = row[0] 
+
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
   reg_date_time = row[:regdate].split(' ')
   date, time = reg_date_time
   
   normalized_date = "#{fix_date(date)} #{fix_time(time)}"
   parsed_time = Time.strptime(normalized_date, "%m/%d/%Y %H:%M")
-  hour[j] = parsed_time.hour
+  hours[j] = parsed_time.hour
   j += 1
+
+  days << parsed_time.strftime("%A")
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
+
 end
-p hour
-  
+
+
+
+
+
+
+hours_hash = hours.reduce(Hash.new(0)) do |result, hour|
+  result[hour] = result[hour] + 1
+  result
+end
+
+max_value = hours_hash.values.max
+
+hour_maximum = []
+hours_hash.each do |key, value|
+  if max_value == value
+    hour_maximum << key
+  end
+end
+
+puts "the hours that we can target are #{hour_maximum} and day can be #{days.max}"
+
